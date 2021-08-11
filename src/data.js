813342049da1dd25e868e3c3ba8d779e5adfa189
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2021-08-11 12:51:17 +0600

    initialized

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+  donor: [
+    {
+      _id: "01",
+      name: "A.S.S.M.Q-E-Elahy",
+      age: 24,
+      bloodGroup: "O+",
+      lastDonate: "22/04/21",
+      mobile: "01913093116",
+      image: "",
+    },
+    {
+      _id: "02",
+      name: "Faheem Hussain",
+      age: 24,
+      bloodGroup: "A+",
+      lastDonate: "22/04/21",
+      mobile: "01841551123",
+      image: "",
+    },
+  ],
+};
+
+export default data;
